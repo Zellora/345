738ac40ef447e9ab75f4451a6415927d081693a3
TemaKrisa <btsi1@outlook.com>
2022-11-26 12:10:15 +0300

    comment

---
+++ Podgotovka 2/Podgotovka 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Podgotovka 2
+//
+//  Created by 35кабинет 6 on 26.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
